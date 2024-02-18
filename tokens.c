5a9b7ca2ecd341736e262a81fa23140bd325e749
Monarch <monarchofaether@gmail.com>
2024-02-18 21:35:52 +0300

    tokens.c removed

--- tokens.c
+++
@@ -1,9 +0,0 @@
-/**
- * _tokenized - breaks up the strings into tokens using strtok
- * and stores each token in an array
- * Return: pointer to array of tokenized string
- **/
-
-char * _tokenized(char *lineptr)
-{
-}
