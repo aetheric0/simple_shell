fa956e34c3a7a4b7d4515abbae2835ce2fae06fd
mornark <monarchofaether@gmail.com>
2024-03-01 23:08:42 +0300

    environment taking care of

--- main.c
+++ main.c
@@ -30,7 +30,6 @@ void printenv(char **env)
 	int i = 0;
 
 	for (i = 0; env[i] != NULL; i++)
-		write(STDOUT_FILENO, env[i], _strlen(env[i]));
-	write(STDOUT_FILENO, "\n", 1);
+	printf("%s\n", env[i]);
 
 }
